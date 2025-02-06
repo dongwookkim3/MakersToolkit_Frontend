18f824f414a3a2cd7fe030225538597912cf1845
dongwookkim3 <dongwook.3.kim@gmail.com>
2025-02-06 14:31:15 +0000

    firebase 연결

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,29 @@
+"use client";
+
+import { database } from "../firebaseConfig";
+import { ref, onValue } from "firebase/database";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const dataRef = ref(database, "1301/");
+
+    // 실시간 데이터 가져오기
+    onValue(dataRef, (snapshot) => {
+      if (snapshot.exists()) {
+        setData(snapshot.val());
+      } else {
+        setData("데이터 없음");
+      }
+    });
+  }, []);
+
   return (
-    <>
-      hello
-    </>
+    <div>
+      <h1>실시간 데이터</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
   );
 }
